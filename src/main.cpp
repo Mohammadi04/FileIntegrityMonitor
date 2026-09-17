3d66cefb6ef050a6700621fbc89949d97925c862
Tanzim Islam <tanzimislam@Tanzims-MacBook-Air-3.local>
2026-09-17 15:37:07 -0400

    Add recursive file scanning with file size and sample files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <filesystem>
 
-int main(){
-	std::cout << "File Integrity Monitor\n";
-	return 0;
+namespace fs = std::filesystem;
+
+int main(int argc, char* argv[]){
+    if(argc != 2){
+        std::cerr << "Usage: ./fim <directory>\n.";
+        return 1;
+    }
+
+    fs::path directory = argv[1];
+
+    try{
+        if(!fs::is_directory(directory)){
+            std::cerr << "Error: path is not a directory.\n";
+            return 1;
+        }
+        std::size_t file_count = 0;
+
+        for(const auto& entry:
+        fs::recursive_directory_iterator(directory)){
+            if(entry.is_symlink()){
+                continue;
+            }
+
+            if(entry.is_regular_file()){
+                std::cout << entry.path().string() << " | " << entry.file_size() << "bytes\n";
+                ++file_count;
+            }
+        }
+        std::cout << "Scan complete. Files found: "
+                  << file_count << '\n';
+    }catch(const fs::filesystem_error& error){
+        std::cerr << "Scan failed: " << error.what() << '\n';
+        return 1;
+    }
+    return 0;
 }
